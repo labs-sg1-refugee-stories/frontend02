182ed038ed9e934479e2ad0598742a6c2cb1ffa2
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-06-04 16:35:41 -0700

    production backend changed to https://refugee-api.herokuapp.com/

--- refugee-stories/src/utils/urls.js
+++ refugee-stories/src/utils/urls.js
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://refugee-stories-api.herokuapp.com/"
+    ? "https://refugee-api.herokuapp.com/"
     : "http://localhost:8000";
 const authURL = `${baseURL}/auth`;
 const apiURL = `${baseURL}/api`;
