42ba90a8b5ae908943e570872ecee1ab03ba385e
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-07-02 13:48:28 -0700

    adds photos to single story; needs styling; fixes allowComments check

--- src/views/pages/SingleStory.js
+++ src/views/pages/SingleStory.js
@@ -48,16 +48,16 @@ const CommentsWrapper = styled.div`
   .form-control {
     display: block;
     width: 100%;
-    height: calc(1.5em + .75rem + 2px);
-    padding: .375rem .75rem;
+    height: calc(1.5em + 0.75rem + 2px);
+    padding: 0.375rem 0.75rem;
     font-weight: 400;
     line-height: 1.5;
     color: #495057;
     background-color: #fff;
     background-clip: padding-box;
     border: 1px solid #ced4da;
-    border-radius: .25rem;
-    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    border-radius: 0.25rem;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   }
 
   .form-control:focus {
@@ -65,14 +65,17 @@ const CommentsWrapper = styled.div`
     background-color: #fff;
     border-color: #80bdff;
     outline: 0;
-    box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
   }
 
   textarea.form-control {
     height: auto;
   }
 
-  [type=button]:not(:disabled), [type=reset]:not(:disabled), [type=submit]:not(:disabled), button:not(:disabled) {
+  [type="button"]:not(:disabled),
+  [type="reset"]:not(:disabled),
+  [type="submit"]:not(:disabled),
+  button:not(:disabled) {
     cursor: pointer;
   }
 
@@ -88,19 +91,22 @@ const CommentsWrapper = styled.div`
     user-select: none;
     background-color: transparent;
     border: 1px solid transparent;
-    padding: .375rem .75rem;
+    padding: 0.375rem 0.75rem;
     line-height: 1.5;
-    border-radius: .25rem;
-    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   }
 
-  .btn-primary.focus, .btn-primary:focus {
-    box-shadow: 0 0 0 0.2rem rgba(38,143,255,.5);
+  .btn-primary.focus,
+  .btn-primary:focus {
+    box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
   }
 
-  .btn.focus, .btn:focus {
+  .btn.focus,
+  .btn:focus {
     outline: 0;
-    box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
   }
 
   .btn-primary {
@@ -141,15 +147,15 @@ class SingleStory extends React.Component {
     this.props.history.push("/admin_stories_list");
   }
 
-  textChangeHandler = (event) => {
-    event.preventDefault()
-    this.setState({ [event.target.name]: event.target.value })
-  }
+  textChangeHandler = event => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-  onSubmitComment = (event) => {
-    event.preventDefault()
-    this.props.addComment(this.state)
-  }
+  onSubmitComment = event => {
+    event.preventDefault();
+    this.props.addComment(this.state);
+  };
 
   render() {
     return (
@@ -158,41 +164,63 @@ class SingleStory extends React.Component {
         <HeroWrapper>
           <StoryWrapper>
             <div>
-              {this.props.singleStory ?
+              {this.props.singleStory ? (
                 <div>
                   <h1>{this.props.singleStory.title}</h1>
                   <h4>{this.props.singleStory.name}</h4>
                   <h4>{this.props.singleStory.country}</h4>
                   <p>{this.props.singleStory.storytext}</p>
+                  {this.props.singleStory.photoUrl && (
+                    <img src={this.props.singleStory.photoUrl} alt="story" />
+                  )}
+                  {this.props.singleStory.authorUrl && (
+                    <img src={this.props.singleStory.authorUrl} alt="author" />
+                  )}
                 </div>
-               : null
-              }
+              ) : null}
             </div>
 
             <CommentsWrapper>
-              <hr/>
+              <hr />
 
               <h4>Comments</h4>
 
-              {(this.props.singleStory && this.props.singleStory.allowComments === 1) ?
+              {this.props.singleStory &&
+              this.props.singleStory.allowComments === true ? (
                 <form onSubmit={this.onSubmitComment}>
                   <div className="form-group">
                     <label htmlFor="input-name">Name</label>
-                    <input type="text" name="name" className="form-control" onChange={this.textChangeHandler}></input>
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control"
+                      onChange={this.textChangeHandler}
+                    />
                   </div>
                   <div className="form-group">
                     <label htmlFor="input-text">Message</label>
-                    <textarea rows="4" name="text" className="form-control" onChange={this.textChangeHandler} required={true}></textarea>
+                    <textarea
+                      rows="4"
+                      name="text"
+                      className="form-control"
+                      onChange={this.textChangeHandler}
+                      required={true}
+                    />
                   </div>
-                  <button type="submit" className="btn btn-block btn-primary">Add Comment</button>
-                </form> : null}
-
-              {(this.props.comments) ? this.props.comments.map((comment, index) =>
-                <div key={index}>
-                  <h6>{comment.name}</h6>
-                  <p>{comment.text}</p>
-                </div>
+                  <button type="submit" className="btn btn-block btn-primary">
+                    Add Comment
+                  </button>
+                </form>
               ) : null}
+
+              {this.props.comments
+                ? this.props.comments.map((comment, index) => (
+                    <div key={index}>
+                      <h6>{comment.name}</h6>
+                      <p>{comment.text}</p>
+                    </div>
+                  ))
+                : null}
             </CommentsWrapper>
           </StoryWrapper>
         </HeroWrapper>
@@ -203,7 +231,7 @@ class SingleStory extends React.Component {
 
 const mapStateToProps = state => ({
   singleStory: state.singleStory,
-  comments: state.comments
+  comments: state.comments,
 });
 
 export default connect(
